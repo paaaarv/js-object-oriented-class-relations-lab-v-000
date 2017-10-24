0aed6617b6f46464728e8a34c4193de2fd6485f9
Parv Kumar <parvathikumar92@gmail.com>
2017-10-24 19:38:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let driverId = 0
+let passengerId = 0
 let store = {drivers:[], passengers: []}
 class Driver{
   constructor(name){
@@ -6,5 +7,13 @@ class Driver{
     this.id = ++driverId
     store.drivers.push(this)
   }
+}
+
+class Passenger{ 
+    constructor(name){
+      this.name = name 
+      this.id = ++passengerId 
+      store.passengers.push(this)
+    }
 
 }
