191b4e193c8e8ea0b00dd62d0233ed2bf6c62b65
Parv Kumar <parvathikumar92@gmail.com>
2017-10-24 19:26:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 let driverId = 0
-class Driver{ 
+class Driver{
 
   constructor(name){
-    this.name = name 
-    this.id = ++driverId 
+    this.name = name
+    this.id = ++driverId
     
-    
+
+  }
+  let storeArr = []
+  store(){ 
+    storeArr.push(this)
   }
 }
