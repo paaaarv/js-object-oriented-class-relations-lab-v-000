448c11a18d3c43411758e559c5b8f6ffbeee35a1
Parv Kumar <parvathikumar92@gmail.com>
2017-10-24 19:41:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,11 +19,14 @@ class Passenger{
 }
 
 class Trip{
-  constructor(driver){
+  constructor(driver,passenger){
     this.id = ++tripId
     store.trips.push(this)
     if (driver){
       this.driverId = driver.id
+    }
+    if (passenger){
+      this.passengerId = passenger.id
     }
   }
 }
