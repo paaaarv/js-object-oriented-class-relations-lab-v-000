89133add4ecbf0aa70bc91c4ca10946c7a05f3fb
Parv Kumar <parvathikumar92@gmail.com>
2017-10-24 19:56:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,12 +30,11 @@ class Trip{
     }
     store.trips.push(this)
   }
-    
+
 
   passenger(){
     return store.passengers.find(function(passenger){
       return passenger.id === this.passengerId
     })
   }
-}
 }
